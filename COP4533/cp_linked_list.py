6f0641b72513bbda0a1e09c4cfdcb31d012101d0
Adam Burford <adamburford@gmail.com>
2020-11-08 03:08:31 -0500

    Bug fix, optimization, tests

--- COP4533/cp_linked_list.py
+++ COP4533/cp_linked_list.py
@@ -2,15 +2,12 @@
 #COP4533
 #Section 3594
 
-from collections import deque
-import timeit
-import random
-
 class Node:
 
     def __init__(self,initdata):
         self.data = initdata
         self.next = None
+
 
 class LinkedList():
 
@@ -18,9 +15,11 @@ class LinkedList():
 		self.head = None
 		self.tail = None
 
+
 	def __iter__(self):
 		self.iter_node = self.head
 		return self
+
 
 	def __next__(self):
 		if self.iter_node == None:
@@ -31,25 +30,34 @@ class LinkedList():
 		self.iter_node = self.iter_node.next
 		
 		return data
-	
+
+
+	def element_at(self, index):
+		'''Returns item at specified index'''
+		return self.node_at(index).data
+
+
+	def node_at(self, index):
+		current_node = self.head
+		for i in range(index):
+			current_node = current_node.next
+		return current_node
+
+
 	def isEmpty(self):
 		'''Returns True if Linked List is empty, False if it contains data'''
 		return self.head == None
 
+
 	def add(self,item):
 		'''Add new item at first position in the list'''
-		new_node = Node(item)
-		new_node.next = self.head
-		self.head = new_node
-		if self.tail == None:
-			self.tail = new_node
+		self.insert(item, 0)
+
 
 	def size(self):
 		'''Returns number of nodes'''
 		return sum(1 for _ in self)
 
-	def __len__(self):
-		return self.size()
 
 	def search(self, item):
 		'''Returns true if item is found in list, false otherwise'''
@@ -63,6 +71,7 @@ class LinkedList():
 				current = current.next
 
 		return found
+
 
 	def remove(self, item):
 		'''Removes first instance of item found in list'''
@@ -83,12 +92,14 @@ class LinkedList():
 			if current.next == None:
 				tail = previous
 
+
 	def append(self, item):
 		'''Adds item to the end of the list'''
 		new_node = Node(item)
 
 		tail.setNext(new_node)
 		tail = new_node
+
 
 	def insert(self, item, index):
 		'''Inserts item at specified index in list'''
@@ -107,21 +118,6 @@ class LinkedList():
 			self.tail == new_node
 		previous_node.next = new_node
 
-	def element_at(self, index):
-		'''Returns item at specified index'''
-		return self.node_at(index).data
-
-
-	def node_at(self, index):
-		current_node = self.head
-		for i in range(index):
-			current_node = current_node.next
-		return current_node
-
-	def clear():
-		'''Removes all items, good luck garbage collector'''
-		self.head = None
-		self.tail = None
 
 	def pop(self, index = None):
 		'''Removes and returns item at specified index in the list
@@ -159,56 +155,27 @@ class LinkedList():
 
 		return current_node
 
+
 	def removeAt(self, index):
 		'''Removes node at specified index'''
 
-	def __len__(self):
-		return self.size()
-
-	def __getitem__(self, key):
-		return self.element_at(key)
-
-	def __setitem__(self, key):
-		self.insert(key)
-
-	def __delitem__(self, key):
-		self.pop(key)
-
-	def __contains__(self, item):
-		return self.search(item)
 
 	def __str__(self):
 		return "[" + ", ".join((str(x) for x in self)) + "]"
 		
-def fill_list(list, count = 1000000):
-	for x in range(count):
-		list.append(random.randrange(1,101))
 
 def main():
 
-	time = timeit.timeit(setup = "python_list = []", stmt = "fill_list(python_list)", number = 3, globals=globals())
-	print(time)
-
-	mysetup = '''test'''
-
-	python_deque = deque()
-	fill_list(python_deque, 100)
-	print(python_deque)
-
-	my_list = LinkedList()
+	myList = LinkedList()
 
 	for x in range(10):
-		my_list.add(x + 1)
+		myList.add(x + 1)
 
-	print("TEST LIST: " + str(my_list))
-	for x in my_list:
-		print(x)
+	print(myList)
+	print(myList.element_at(0))
 
-	myList = sorted(my_list)
-
-	print("Sorted: " + str(myList))
-	for x in myList:
-		print(x)
+	myList = sorted(myList)
+	print(myList)
 
 	test1 = LinkedList()
 	test1.add(0)
@@ -246,7 +213,6 @@ def main():
 
 if __name__ == "__main__":
 	main()
-
 
 
 
